9b833e2b41db34f503bef01a9373e434ca99b37f
Mohammed-Fares-Git <mohammedfaresgit@gmail.com>
2024-02-29 17:47:34 +0100

    model

---
+++ lib/Exercice.dart
@@ -0,0 +1,9 @@
+
+
+class Exercice {
+  final String title;
+  final Map<String,List<String>> questionSolutionPair;
+  final String solutionUrl;
+
+  Exercice(this.title, this.questionSolutionPair, this.solutionUrl);
+}
